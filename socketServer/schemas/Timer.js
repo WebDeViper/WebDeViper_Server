04854fbf8fc3882495f1cb8c8625c530450e0f7e
KimSeEun <cocobell3@naver.com>
2023-10-25 14:03:03 +0900

    feat: socket server 폴더구조 만들기

--- socketServer/schemas/Timer.js
+++ socketServer/schemas/Timer.js
@@ -6,7 +6,7 @@ const timerSchema = new Schema({
     nickname: { type: String, required: true, unique: true },
     daily: {
       date: { type: Date },
-      subjects: [
+      data: [
         {
           title: { type: String },
           timer: { type: String },
@@ -21,4 +21,28 @@ timerSchema.add({
   _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
 });
 
+// const newTimerData = {
+//   id: 'alsdud1240',
+//   nickname: '몽구스',
+//   daily: {
+//     date: new Date('2023-10-23'),
+//     subjects: [
+//       {
+//         title: '운영체제',
+//         timer: '4:05:23',
+//       },
+//     ],
+//   },
+// };
+
+// const newTimer = new Timer(newTimerData);
+
+// newTimer
+//   .save()
+//   .then(() => {
+//     console.log('새로운 timer 데이터가 MongoDB에 추가되었습니다.');
+//   })
+//   .catch(err => {
+//     console.error('데이터 추가 중 오류 발생: ', err);
+//   });
 module.exports = mongoose.model('timer', timerSchema);
