5468c462fa85423204511830bcc6532787d01a0b
KimSeEun <cocobell3@naver.com>
2023-10-26 06:32:24 +0900

    feat: todo CRUD 완성

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const routerUser = require('./routerUser');
 const routerNotice = require('./routerNotice');
+const routerTodo = require('./routerTodo');
 // router.use('/api/user', userRouter);
 router.use('/api', routerNotice);
+router.use('/api', routerTodo);
 module.exports = router;
