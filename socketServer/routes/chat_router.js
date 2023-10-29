f98b96bbcf5ca4f76356a530a0873f85d7a556ac
KimSeEun <cocobell3@naver.com>
2023-10-29 21:31:24 +0900

    feat: mongodb 스키마 수정(group_id 추가)

--- socketServer/routes/chat_router.js
+++ socketServer/routes/chat_router.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const router = express.Router();
-module.exports = router;
+const controller = require('../controller/ctrTimer.js');
+
+router.get('/timer', controller.getTimer);
+router.module.exports = router;
