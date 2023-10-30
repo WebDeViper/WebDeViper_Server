a9e334f5531bc1013814a1ed14b20dd966c25908
hotdog7778 <rlaxorbs159@gmail.com>
2023-10-30 17:28:14 +0900

    refactor: Notice 라우터에 로그인토큰 검사 추가

--- server/routes/routerNotice.js
+++ server/routes/routerNotice.js
@@ -5,9 +5,9 @@ const { isAdmin } = require('../middlewares/isAdmin');
 const controller = require('../controller/ctrNotice');
 // 컨트롤러
 router.get('/notices', verifyJwtToken, controller.getNotice);
+router.get('/notice/:notice_id', verifyJwtToken, controller.getNoticeDetail);
 router.post('/notice', verifyJwtToken, isAdmin, controller.postNotice);
 router.patch('/notice', verifyJwtToken, isAdmin, controller.patchNotice);
 router.delete('/notice', verifyJwtToken, isAdmin, controller.deleteNotice);
-router.get('/notice/:notice_id', controller.getNoticeDetail);
 
 module.exports = router;
