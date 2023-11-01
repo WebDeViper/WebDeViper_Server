145420a59e1e3d387dcaa3ced18fb3e3946105e7
hongminyeong <minyung1240@daum.net>
2023-11-01 21:40:29 +0900

    refactor: user가 방을 나갔을 때 메시지 보내고 room스키마 member array에서 제거

--- socketServer/utils/io.js
+++ socketServer/utils/io.js
@@ -9,7 +9,7 @@ module.exports = function (io) {
     Room.find({
       $or: [{ room: '공무원' }, { room: '취준생' }, { room: '연습생' }],
     }).then(rooms => {
-      console.log('방잇나여', rooms);
+      // console.log('방잇나여', rooms);
       if (rooms.length === 0) {
         Room.insertMany([
           {
@@ -27,7 +27,7 @@ module.exports = function (io) {
         ])
           .then(() => console.log('ok'))
           .catch(error => console.error(error));
-        console.log('방 이미 잇음');
+        // console.log('방 이미 잇음');
       }
     });
 
@@ -36,11 +36,6 @@ module.exports = function (io) {
       try {
         name = userName;
         const user = await userController.saveUser(userName, socket.id);
-        // const welcomeMessage = {
-        //   chat: `${user.nick_name}님이 입장하셨습니다.`,
-        //   user: { id: null, nick_name: 'system' },
-        // };
-        // io.emit('message', welcomeMessage);
         if (user) {
           //user가 null이 아니라면
           cb({ isOk: true, data: user });
@@ -54,9 +49,9 @@ module.exports = function (io) {
     socket.emit('rooms', await userController.getAllRooms()); // 룸 리스트 보내기
     socket.on('joinRoom', async (joinUser, rid, cb) => {
       try {
-        console.log('조인룸 유저정보', joinUser);
+        // console.log('조인룸 유저정보', joinUser);
         const saveuser = await userController.saveUser(joinUser.nick_name, socket.id);
-        console.log('유저정보는', saveuser);
+        // console.log('유저정보는', saveuser);
         const user = await userController.checkUser(socket.id); // 일단 유저정보들고오기
         await userController.joinRoom(rid, user); // 1~2작업
         socket.join(user.room.toString()); //3 작업
@@ -64,6 +59,7 @@ module.exports = function (io) {
           chat: `${user.nick_name} is joined to this room`,
           user: { id: null, name: 'system' },
         };
+
         io.to(user.room.toString()).emit('message', welcomeMessage); // 4 작업
         const room = io.emit('rooms', await userController.getAllRooms()); // 5 작업
         cb({ ok: true, data: room });
@@ -73,41 +69,21 @@ module.exports = function (io) {
     });
     socket.on('leaveRoom', async (_, cb) => {
       try {
-        const user = await checkUser(socket.id);
-        await roomController.leaveRoom(user);
+        const user = await userController.checkUser(socket.id);
+        console.log('유저는 ', user);
+        await userController.leaveRoom(user);
         const leaveMessage = {
-          chat: `${user.name} left this room`,
+          chat: `${user.nick_name} left this room`,
           user: { id: null, name: 'system' },
         };
         socket.broadcast.to(user.room.toString()).emit('message', leaveMessage); // socket.broadcast의 경우 io.to()와 달리,나를 제외한 채팅방에 모든 맴버에게 메세지를 보낸다
-        io.emit('rooms', await roomController.getAllRooms());
+        io.emit('rooms', await userController.getAllRooms());
         socket.leave(user.room.toString()); // join했던 방을 떠남
         cb({ ok: true });
       } catch (error) {
         cb({ ok: false, message: error.message });
       }
     });
-    // socket.emit('rooms', await roomController.getAllRooms()); // 룸 리스트 보내기
-    // 유저관련
-    // socket.on('login', async (userName, cb) => {
-    //   try {
-    //     name = userName;
-    //     const user = await userController.saveUser(userName, socket.id);
-    //     const welcomeMessage = {
-    //       chat: `${user.nick_name}님이 입장하셨습니다.`,
-    //       user: { id: null, nick_name: 'system' },
-    //     };
-    //     io.emit('message', welcomeMessage);
-    //     if (user) {
-    //       //user가 null이 아니라면
-    //       cb({ isOk: true, data: user });
-    //     } else {
-    //       cb({ isOk: false, message: '잘못된 접근입니다.' });
-    //     }
-    //   } catch (error) {
-    //     cb({ isOk: false, error: error.message });
-    //   }
-    // });
 
     socket.on('getChatLog', async cb => {
       try {
