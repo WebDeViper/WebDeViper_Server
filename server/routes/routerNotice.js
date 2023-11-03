817403ff6ec42739d366fa1b4397d8c21c0d8e47
KimSeEun <cocobell3@naver.com>
2023-11-04 01:45:55 +0900

    fix: notice에서 관리자여부 알려서 프론트측에서 수정/삭제 가능하도록, 관리자 아닐경우 예외처리

--- server/routes/routerNotice.js
+++ server/routes/routerNotice.js
@@ -4,7 +4,7 @@ const { verifyJwtToken } = require('../middlewares/jwt/jwt');
 const { isAdmin } = require('../middlewares/isAdmin');
 const controller = require('../controller/ctrNotice');
 // 컨트롤러
-router.get('/notices', verifyJwtToken, controller.getNotice);
+router.get('/notices', controller.getNotice);
 router.get('/notice/:notice_id', verifyJwtToken, controller.getNoticeDetail);
 router.post('/notice', verifyJwtToken, isAdmin, controller.postNotice);
 router.patch('/notice', verifyJwtToken, isAdmin, controller.patchNotice);
