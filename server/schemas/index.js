c589cfc20253700c901986f0b5f1e90386863a2b
HongMinYeong <minyung1240@daum.net>
2023-11-03 22:37:33 +0900

    fix: 데이터베이스 스키마 수정
    group스키마에 password 삭제
    user 스키마에 pending_groups 배열안에 is_approve 속성 삭제
    group스키마에 join_requests 배열안에 is_approve 속성 삭제

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -3,7 +3,6 @@ const { User, Group } = require('./schema');
 const { MONGO_ID, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT } = process.env;
 const MONGO_URL = `mongodb+srv://${MONGO_ID}:${MONGO_PASSWORD}@${MONGO_HOST}`;
 
-
 const connect = () => {
   // 개발 환경에서만 몽구스가 생성하는 쿼리내용 확인
   if (process.env.NODE_ENV !== 'production') {
@@ -11,8 +10,8 @@ const connect = () => {
   }
 
   mongoose.connect(MONGO_URL, {
-    // dbName: 'viper_local',
-    dbName: 'seeun_apiserver_test',
+    dbName: 'min0_test',
+    // dbName: 'seeun_apiserver_test',
     // useNewUrlParser: true, // 별 의미 없음
   });
 
