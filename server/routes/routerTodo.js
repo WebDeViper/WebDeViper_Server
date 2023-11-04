de6ee619cee5490f96e53bffaff975b77c2779d5
KimSeEun <cocobell3@naver.com>
2023-11-04 13:32:19 +0900

    fix: todo schema에서 done삭제 및 CRUD에서도 done관련 삭제, verifyJwtToken router 수정

--- server/routes/routerTodo.js
+++ server/routes/routerTodo.js
@@ -7,7 +7,7 @@ const { verifyJwtToken } = require('../middlewares/jwt/jwt');
 // router.post('/todo_list/:user_id', verifyJwtToken, controller.postTodo);
 // router.patch('/todo_lists/:user_id', verifyJwtToken, controller.patchTodo);
 // router.delete('/todo_lists/:user_id', verifyJwtToken, controller.deleteTodo);
-router.get('/todo_lists', controller.getTodoList);
+router.get('/todo_lists', verifyJwtToken, controller.getTodoList);
 router.post('/todo_list', verifyJwtToken, controller.postTodo);
 router.patch('/todo_lists', verifyJwtToken, controller.patchTodo);
 router.delete('/todo_list', verifyJwtToken, controller.deleteTodo);
