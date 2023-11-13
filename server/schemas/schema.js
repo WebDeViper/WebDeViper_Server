a2d4749e3dc34b300f583748219db107dad022aa
KimSeEun <cocobell3@naver.com>
2023-11-13 21:17:11 +0900

    fix: chat schema change user_id type, add timer Schema

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -8,7 +8,7 @@ const chatSchema = new Schema({
   receiver: { type: String, default: null },
   send_at: { type: Date, default: Date.now },
   user: {
-    user_id: { type: Schema.Types.ObjectId, ref: 'User' }, // 사용자 id
+    user_id: { type: String }, // 사용자 id
     name: { type: String },
   },
   room_id: { type: Schema.Types.ObjectId, ref: 'Room' },
@@ -23,7 +23,24 @@ const notificationSchema = new Schema({
   updated_at: { type: Date, default: Date.now },
 });
 
+//타이머 스키마
+const timerSchema = new Schema({
+  user_id: { type: String },
+  total_time: { type: Number, default: 0 },
+  is_running: { type: Boolean, default: false },
+  daily: {
+    date: { type: Date, default: Date.now }, // 오늘 날짜
+    data: [
+      {
+        title: { type: String }, // 과목
+        timer: { type: Number, default: 0 }, // 초 단위로 공부한 시간
+      },
+    ],
+  },
+});
+
+const Timer = mongoose.model('Timer', timerSchema, 'timer');
 const Chat = mongoose.model('Chat', chatSchema, 'chat');
 const Notification = mongoose.model('Notification', notificationSchema, 'notification');
 
-module.exports = { Chat, Notification, mongoose };
+module.exports = { Chat, Notification, Timer, mongoose };
