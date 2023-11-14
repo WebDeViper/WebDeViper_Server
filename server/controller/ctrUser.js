4303e57d9c0b246e6269dc049ae5c0dd152fff42
hotdog7778 <rlaxorbs159@gmail.com>
2023-11-14 20:41:49 +0900

    refactor: User API 시퀄라이즈 적용 완료

--- server/controller/ctrUser.js
+++ server/controller/ctrUser.js
@@ -12,7 +12,7 @@ exports.getUserInfo = async (req, res) => {
   try {
     const targetUser = req.params.userId;
 
-    const userInfo = await User.findByPk(targetUser);
+    const userInfo = await User.findById(targetUser);
 
     res.send({
       isSuccess: true,
@@ -150,18 +150,36 @@ exports.registerUser = async (req, res) => {
     // 유저 이메일, 패스워드, 닉네임, 카테고리를 받는다.
     const { email, password, nickName, category } = req.body;
 
-    // 닉네임의 중복만 확인한다.
-    const nickDuplicate = await User.findAll({ where: { nick_name: nickName } });
-    if (nickDuplicate.length !== 0) {
+    // 닉네임의 중복 확인한다.
+    const nickDuplicate = await User.count({
+      where: {
+        [Op.and]: [{ provider: 'local' }, { nick_name: nickName }],
+      },
+    });
+    if (nickDuplicate !== 0) {
       console.log('이미 해당 닉네임이 있음');
       return res.status(409).send({
+        isSuccess: false,
         message: '이미 존재하는 닉네임 입니다.',
+      });
+    }
+
+    // 이메일의 중복 확인한다.
+    const emailDuplicate = await User.count({
+      where: {
+        [Op.and]: [{ provider: 'local' }, { email: email }],
+      },
+    });
+    if (emailDuplicate !== 0) {
+      console.log('이미 해당 이메일이 있음');
+      return res.status(409).send({
+        isSuccess: false,
+        message: '이미 존재하는 이메일 입니다.',
       });
     }
 
     // 패스워드 암호화
     const hashedPassword = hashPassword(password);
-    console.log('>>>>>>', hashedPassword);
 
     // 데이터베이스에 유저 정보 저장
     let userInfo = {
@@ -198,42 +216,38 @@ exports.login = async (req, res) => {
   const { email, password } = req.body;
 
   // DB에 조회
-  const exUser = await User.findAll({
-    where: { email },
+  const exUser = await User.findOne({
+    where: {
+      [Op.and]: [{ provider: 'local' }, { email: email }],
+    },
   });
 
   // 이메일 확인
-  if (exUser.length === 0) {
+  if (!exUser) {
     return res.status(409).send({
       isSuccess: false,
-      message: '이메일 오류',
+      message: '찾을 수 없는 이메일 입니다.',
     });
   }
 
   // 암호 확인
-  let passCheck;
-  let userInfo;
-  for (const user of exUser) {
-    passCheck = comparePassword(password, user.password);
-    if (passCheck) {
-      userInfo = {
-        id: user.user_id,
-        category: user.category,
-        nickName: user.nick_name,
-        profileImg: user.image_path,
-        email: user.email,
-        statusMsg: user.status_message,
-        isServiceAdmin: user.is_admin,
-      };
-    }
-  }
-
+  let passCheck = comparePassword(password, exUser.password);
   if (!passCheck) {
     return res.status(409).send({
       isSuccess: false,
       message: '패스워드 오류',
     });
   }
+
+  let userInfo = {
+    id: exUser.user_id,
+    category: exUser.category,
+    nickName: exUser.nick_name,
+    profileImg: exUser.image_path,
+    email: exUser.email,
+    statusMsg: exUser.status_message,
+    isServiceAdmin: exUser.is_admin,
+  };
 
   // 로그인 처리를 하기위해 jwt 발급
   const accessToken = generateJwtToken(userInfo);
@@ -265,8 +279,6 @@ exports.patchUser = async (req, res) => {
     if (nickName) {
       // 닉네임 중복검사
       const isDuplicate = await duplicateCheck(User, 'nick_name', nickName);
-
-      console.log('@@@@@', isDuplicate);
 
       if (isDuplicate) {
         return res.status(409).send({
@@ -314,7 +326,6 @@ exports.patchUser = async (req, res) => {
 
 exports.userNickDuplicateCheck = async (req, res) => {
   try {
-    console.log(req.params);
     // 닉네임 중복확인
     const isDuplicate = await duplicateCheck(User, 'nick_name', req.params.nickName);
 
@@ -334,17 +345,14 @@ exports.userNickDuplicateCheck = async (req, res) => {
 exports.userProfileImgUpload = async (req, res) => {
   try {
     const currentUserId = res.locals.decoded.userInfo.id;
-    // const currentUserId = '6544cc034f18de981a274777';
+
     // 서버에 저장된 파일 이름
     const { filename } = req.file;
     // path == 이미지를 받을 수 있는 URL
     const path = `/api/static/profileImg/${filename}`;
 
-    // 이미지 경로 수정해서 토큰 재생성
-    // 응답값에 전달하고 리액트에서 상태관리 하면 될듯
-
     // user 테이블에 이미지 경로 저장
-    await User.updateOne({ _id: currentUserId }, { user_profile_image_path: path });
+    await User.update({ image_path: path }, { where: { user_id: currentUserId } });
 
     // 업로드 성공 응답
     res.status(200).send({
