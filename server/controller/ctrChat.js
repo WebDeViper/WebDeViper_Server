5930c0958091f9265760c3815b57248f159fa543
HongMinYeong <minyung1240@daum.net>
2023-11-21 15:50:38 +0900

    feat: 귓속말 채팅 기능 추가

--- server/controller/ctrChat.js
+++ server/controller/ctrChat.js
@@ -1,26 +1,27 @@
-const { User, Group, Chat, mongoose } = require('../schemas/schema');
+const { Chat } = require('../schemas/schema');
+const { User, Group, UserGroupRelation } = require('../models');
 
-// 사용자 정보를 저장하거나 업데이트하는 함수
-exports.saveUser = async (userName, socketid) => {
-  try {
-    // 이미 있는 사용자인지 확인
-    console.log('userName은 컨트롤러 함수에서 ', userName);
-    const user = await User.findOne({ nick_name: userName });
-    console.log('user는', user);
-    // 사용자가 존재하지 않으면 새 사용자 정보를 생성
-    if (!user) {
-      return user;
-    }
-    // 이미 존재하는 사용자의 연결 정보 (token)를 업데이트하고 온라인 상태로 설정
-    user.token = socketid;
-    // user.online = true;
+// // 사용자 정보를 저장하거나 업데이트하는 함수
+// exports.saveUser = async (userName, socketid) => {
+//   try {
+//     // 이미 있는 사용자인지 확인
+//     console.log('userName은 컨트롤러 함수에서 ', userName);
+//     const user = await User.findOne({ nick_name: userName });
+//     console.log('user는', user);
+//     // 사용자가 존재하지 않으면 새 사용자 정보를 생성
+//     if (!user) {
+//       return user;
+//     }
+//     // 이미 존재하는 사용자의 연결 정보 (token)를 업데이트하고 온라인 상태로 설정
+//     user.token = socketid;
+//     // user.online = true;
 
-    await user.save();
-    return user;
-  } catch (err) {
-    console.error(err);
-  }
-};
+//     await user.save();
+//     return user;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 exports.deleteUser = async socketId => {
   try {
     const user = await User.findOne({ token: socketId });
@@ -39,32 +40,32 @@ exports.deleteUser = async socketId => {
     console.error(err);
   }
 };
-exports.isToken = async name => {
-  try {
-    const user = await User.findOne({ nick_name: name });
-    User.findOne({ nick_name: name }, (err, user) => {
-      if (err) {
-        console.error('데이터베이스 쿼리 오류:', err);
-      } else {
-        if (user) {
-          if (user.token) {
-            console.log(`사용자 ${name}의 토큰 값: ${user.token}`);
-            const token = user.token;
-          } else {
-            console.log(`사용자 ${name}는 토큰 값을 가지고 있지 않습니다.`);
-            const token = null;
-          }
-        } else {
-          console.log(`사용자 ${name}를 찾을 수 없음`);
-        }
-      }
-    });
-    console.log('token은 ->', token);
-    return token;
-  } catch (err) {
-    console.error(err);
-  }
-};
+// exports.isToken = async name => {
+//   try {
+//     const user = await User.findOne({ nick_name: name });
+//     User.findOne({ nick_name: name }, (err, user) => {
+//       if (err) {
+//         console.error('데이터베이스 쿼리 오류:', err);
+//       } else {
+//         if (user) {
+//           if (user.token) {
+//             console.log(`사용자 ${name}의 토큰 값: ${user.token}`);
+//             const token = user.token;
+//           } else {
+//             console.log(`사용자 ${name}는 토큰 값을 가지고 있지 않습니다.`);
+//             const token = null;
+//           }
+//         } else {
+//           console.log(`사용자 ${name}를 찾을 수 없음`);
+//         }
+//       }
+//     });
+//     console.log('token은 ->', token);
+//     return token;
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
 
 // 사용자의 토큰 (연결 정보)을 검사하고 해당 사용자를 반환
 exports.checkUser = async joinUser => {
@@ -83,28 +84,26 @@ exports.checkUser = async joinUser => {
 exports.joinRoom = async (roomId, user) => {
   try {
     console.log(roomId);
-    const room = await Group.findById(roomId);
+    const room = await Group.findByPK(roomId);
     if (!room) {
       throw new Error('해당 방이 없습니다.');
     }
+    const isMember = await UserGroupRelation.findOne({
+      where: { group_id: roomId, user_id: user.user_id, request_status: 'a' },
+    });
 
-    if (!room.members.includes(user._id)) {
+    if (!isMember) {
       console.log('해당 그룹의 유저가 아닙니다.');
-    }
-
-    if (!user.rooms.includes(roomId)) {
-      user.rooms.push(roomId);
-      await user.save();
-      console.log('룸 아이디 추가 완료');
+      return false;
     } else {
-      console.log('이미 룸 아이디가 존재합니다.');
+      return true;
     }
   } catch (err) {
     console.error(err);
   }
 };
 // 메시지를 저장하는 함수
-exports.saveChat = async (rid, message, user) => {
+exports.saveChat = async (rid, receiver, message, user) => {
   try {
     // 현재 시각을 UTC 시간대로 얻어옴.
     const utcDate = new Date();
@@ -126,17 +125,28 @@ exports.saveChat = async (rid, message, user) => {
     // const formattedDate = `${hours}:${minutes} [${year}/${month}/${day}]`;
 
     // Chat 모델을 사용하여 새로운 메시지를 생성하고 저장
-    const newMessage = new Chat({
-      chat: message,
-      sender: user.nick_name,
-      // send_at: formattedDate,
-      user: {
-        user_id: user._id,
-        name: user.nick_name,
-      },
-      room_id: rid,
-    });
-    await newMessage.save();
+    if (!receiver) {
+      const newMessage = new Chat({
+        chat: message,
+        sender: { id: user.user_id, name: user.nick_name },
+        room_id: rid,
+      });
+      await newMessage.save();
+    } else {
+      const receiveId = await User.findOne({
+        where: { nick_name: receiver },
+        attributes: ['user_id'],
+      });
+
+      const newMessage = new Chat({
+        chat: message,
+        receiver: { receive_id: receiveId, name: receiver },
+        sender: { id: user.user_id, name: user.nick_name },
+        room_id: rid,
+      });
+      await newMessage.save();
+    }
+
     const msg = {
       chat: message,
       user: { id: null, name: user.nick_name },
@@ -152,6 +162,7 @@ exports.getChatLog = async rid => {
   try {
     const chats = await Chat.find({ room_id: rid });
     console.log('이전 채팅 기록 ->', chats);
+
     // const msg = {
     //   chat: chats.chat,
     //   user: { id: null, name: chats.sender },
@@ -162,20 +173,3 @@ exports.getChatLog = async rid => {
     throw error;
   }
 };
-
-// 모든 채팅방의 목록을 반환하는 함수
-// exports.getAllRooms = async () => {
-//   const roomList = await Group.find({});
-//   console.log('겟룸실행', roomList);
-//   return res.send(roomList);
-// };
-
-// // 사용자가 채팅방을 나가는 함수
-// exports.leaveRoom = async user => {
-//   const room = await Room.findById(user.rooms);
-//   if (!room) {
-//     throw new Error('채팅방을 찾을 수 없음');
-//   }
-//   room.members.remove(user._id);
-//   await room.save();
-// };
--- server/sockets/io.js
+++ server/sockets/io.js
@@ -4,6 +4,9 @@ const { User, Group, Room, mongoose } = require('../schemas/schema');
 module.exports = function (io) {
   const chatSpace = io.of('/chat');
   let name; // 사용자의 이름을 저장하는 변수
+  function updateList() {
+    io.emit('updateNicks', nickObjs); // 전체 사용자 닉네임 모음 객체 전달
+  }
   chatSpace.on('connection', async socket => {
     console.log('client is connected', socket.id);
     // 사용자 로그인을 처리합니다.
@@ -24,35 +27,28 @@ module.exports = function (io) {
     //     cb({ isOk: false, error: error.message });
     //   }
     // });
-
-    // 사용자가 채팅방에 참여하는 것을 처리합니다.
+    //사용자 닉네임 모음 객체
+    const nickObjs = {}; //{socket.id:nick1,socket.id:nick2}
+    // 사용자가 채팅방에 참여하는 것을 처리
     socket.on('joinRoom', async (joinUser, rid, cb) => {
       try {
-        await userController.saveUser(joinUser, socket.id);
         const user = await userController.checkUser(joinUser);
+        const isMember = await userController.joinRoom(rid, user);
+        if (!isMember) {
+          cb({ isOk: false, msg: '해당 그룹의 멤버가 아닙니다.' });
+        } else {
+          nickObjs[socket.id] = joinUser;
+          socket.join(rid.toString());
+          const chatLog = await userController.getChatLog(rid);
+          updateList();
+          cb({ isOk: true, data: chatLog });
+        }
 
-        // user.rooms 초기화
-        user.rooms = [];
-
-        await userController.joinRoom(rid, user);
-
-        // 'user.rooms' 값이 설정된 이후에 'socket.join' 호출
-        socket.join(user.rooms.toString());
-
-        const welcomeMessage = {
-          chat: `${user.nick_name}님이 입장하셨습니다.`,
-          user: { id: null, name: 'system' },
-        };
-        console.log('환영', welcomeMessage);
-        const chatLog = await userController.getChatLog(rid);
-        // const isToken = await userController.isToken(name);
-
-        // if (!isToken) {
-        //token값이 없다면
-        // chatSpace.to(user.rooms.toString()).emit('message', [...chatLog, welcomeMessage]);
-        // } else {
-        // }
-        cb({ isOk: true, data: chatLog });
+        // const welcomeMessage = {
+        //   chat: `${user.nick_name}님이 입장하셨습니다.`,
+        //   user: { id: null, name: 'system' },
+        // };
+        // console.log('환영', welcomeMessage);
       } catch (error) {
         cb({ isOk: false, error: error.message });
       }
@@ -60,40 +56,39 @@ module.exports = function (io) {
     // chatSpace.to(user.rooms.toString()).emit('message', [welcomeMessage]);
 
     // 사용자가 채팅방을 나가는 것을 처리합니다.
-    socket.on('leaveRoom', async (name, cb) => {
+    socket.on('leaveRoom', async (name, rid, cb) => {
       try {
         const user = await userController.checkUser(name);
         // await userController.leaveRoom(user);
-        const leaveMessage = {
-          chat: `${user.nick_name}님이 나가셨습니다.`,
-          user: { id: null, name: 'system' },
-        };
-        socket.broadcast.to(user.rooms.toString()).emit('message', leaveMessage);
-        socket.leave(user.rooms.toString());
+        // const leaveMessage = {
+        //   chat: `${user.nick_name}님이 나가셨습니다.`,
+        //   user: { id: null, name: 'system' },
+        // };
+        // socket.broadcast.to(user.rooms.toString()).emit('message', leaveMessage);
+        socket.leave(rid.toString());
         cb({ isOk: true });
       } catch (error) {
         cb({ isOk: false, message: error.message });
       }
     });
 
-    // 특정 채팅방의 채팅 로그를 가져오는 것을 처리합니다.
-    // socket.on('getChatLog', async (rid, cb) => {
-    //   try {
-    //     const chatLog = await userController.getChatLog(rid);
-    //     cb({ isOk: true, data: chatLog });
-    //   } catch (error) {
-    //     cb({ isOk: false, error: error.message });
-    //   }
-    // });
-
     // 채팅 메시지를 보내는 것을 처리합니다.
-    socket.on('sendMessage', async (rid, name, receivedMessage, cb) => {
+    socket.on('sendMessage', async (rid, name, whisper, receivedMessage, cb) => {
       try {
         console.log('rid는', rid);
         const user = await userController.checkUser(name);
+        let dmSocketId = whisper.who;
+        let receiver;
         if (user) {
-          const message = await userController.saveChat(rid, receivedMessage, user);
-          chatSpace.to(user.rooms.toString()).emit('message', message);
+          if (whisper.who === 'all') {
+            const message = await userController.saveChat(rid, receiver, receivedMessage, user);
+            chatSpace.to(rid.toString()).emit('message', message);
+          } else {
+            receiver = nickObjs[dmSocketId];
+            const message = await userController.saveChat(rid, receiver, receivedMessage, user);
+            chatSpace.to(dmSocketId).emit('dm', message);
+            chatSpace.to(socket.id).emit('dm', message);
+          }
           return cb({ isOk: true });
         }
       } catch (error) {
