6b6065d4d85f4e792fb58180be0c68d45de17109
KimSeEun <cocobell3@naver.com>
2023-11-28 01:06:27 +0900

    fix: notification 에러 수정

--- server/sockets/notification.js
+++ server/sockets/notification.js
@@ -52,7 +52,7 @@ module.exports = function (io) {
     // 필요한 경우 더 많은 연결 이벤트 처리 로직 추가
 
     // 기존에 등록된 모든 'change' 이벤트 리스너를 제거
-    // notificationChangeStreamOfNotice.removeAllListeners('change');
+    notificationChangeStreamOfNotice.removeAllListeners('change');
 
     notificationChangeStreamOfNotice.on('change', change => {
       // notificationChangeStreamOfNotice.removeAllListeners('change');
@@ -61,6 +61,7 @@ module.exports = function (io) {
         io.to(socket.id).emit('newNotice', newNotification);
       }
     });
+    notificationChangeStreamOfGroupRequest.removeAllListeners('change');
 
     notificationChangeStreamOfGroupRequest.on('change', async change => {
       if (change.operationType === 'insert') {
@@ -88,6 +89,7 @@ module.exports = function (io) {
         io.to(leaderSocketId).emit('newGroupRequest', newNotification); //리더이게만 보내는 로직 추가해야 함
       }
     });
+    notificationChangeStreamOfGroupApprove.removeAllListeners('change');
 
     notificationChangeStreamOfGroupApprove.on('change', async change => {
       if (change.operationType === 'insert') {
@@ -98,7 +100,7 @@ module.exports = function (io) {
         //해당 유저의 소켓 아이디
         const requestUserSocketId = requestUserInfo.dataValues.socket_id;
         //보내는 사람:그룹리더로 바꾸기 위한 로직
-        const requestGroupId = newNotification.groupId;
+        const requestGroupId = newNotification.group_id;
         const requestGroupInfo = await Group.findOne({ where: { group_id: requestGroupId } });
         const requestGroupLeaderInfo = await User.findOne({
           where: { user_id: requestGroupInfo.dataValues.leader_id },
@@ -108,7 +110,7 @@ module.exports = function (io) {
         io.to(requestUserSocketId).emit('newGroupApprove', newNotification);
       }
     });
-
+    notificationChangeStreamOfGroupRejection.removeAllListeners('change');
     notificationChangeStreamOfGroupRejection.on('change', async change => {
       if (change.operationType === 'insert') {
         const newNotification = change.fullDocument;
@@ -118,7 +120,7 @@ module.exports = function (io) {
         //해당 유저의 소켓 아이디
         const requestUserSocketId = requestUserInfo.dataValues.socket_id;
         //보내는 사람:그룹리더로 바꾸기 위한 로직
-        const requestGroupId = newNotification.groupId;
+        const requestGroupId = newNotification.group_id;
         const requestGroupInfo = await Group.findOne({ where: { group_id: requestGroupId } });
         const requestGroupLeaderInfo = await User.findOne({
           where: { user_id: requestGroupInfo.dataValues.leader_id },
