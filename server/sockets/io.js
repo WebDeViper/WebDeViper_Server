89cd57cd5a54a10bafa3f9cec612cdeff21134d1
kimseeun <cocobell3@naver.com>
2023-12-05 18:29:12 +0900

    feat: 타이머 수정

--- server/sockets/io.js
+++ server/sockets/io.js
@@ -4,35 +4,20 @@ const { User, Group, Room, mongoose } = require('../schemas/schema');
 const chatModule = require('./chat');
 const timerModule = require('./timer');
 module.exports = function (io) {
-  //group(chat, timer) namespace
   const groupSpace = io.of('/chat');
-  let name; // 사용자의 이름을 저장하는 변수
-  const nickObjs = {}; //{socket.id:nick1,socket.id:nick2}
-  const webRTCObjs = {};
 
-  // function updateList() {
-  //   io.emit('updateNicks', nickObjs); // 전체 사용자 닉네임 모음 객체 전달
-  // }
+  const nickObjs = {};
+
   groupSpace.on('connection', async socket => {
     console.log('사용자 소켓 연결하였씁니다.---------------', socket.id);
     const userId = socket.handshake.auth.userId;
     const groupId = socket.handshake.auth.groupId;
     const userNickName = socket.handshake.auth.userNickName;
 
-    // // webRTC
-    // socket.on('join-zoom', cb => {
-    //   // console.log('>>>>>joinZoomjoinZoom', data);
-    //   console.log(cb);
-    //   // socket.join(groupId);
-    //   // socket.broadcast.to(groupId).emit('user-connected', userId);
-    // });
-
     chatModule(socket, userController, groupId, userNickName);
-
     socket.on('joinRoom', async () => {
       try {
         const user = await userController.checkUser(userNickName);
-
         const isMember = await userController.joinRoom(groupId, user);
         if (!isMember) {
           cb({ isOk: false, msg: '해당 그룹의 멤버가 아닙니다.' });
@@ -41,10 +26,8 @@ module.exports = function (io) {
           if (!nickObjs[groupId]) {
             nickObjs[groupId] = []; // 배열이 없으면 초기화
           }
-
           // 소켓 ID가 이미 존재하는지 확인
           const existingUserIndex = nickObjs[groupId].findIndex(data => data.userId === userId);
-
           if (existingUserIndex === -1) {
             // 존재하지 않으면 새 항목 추가
             //이 경우 user의 타이머도 함께 불러온다
@@ -53,7 +36,6 @@ module.exports = function (io) {
             //   { subject: '수학', time: 70, is_running: 'y' },
             //   userId
             // );
-
             console.log('유저의 타이머 인포!!', userTimerInfo);
             nickObjs[groupId].push({
               userId: userId,
@@ -61,11 +43,9 @@ module.exports = function (io) {
               userProfile: user.image_path,
               totalTime: userTimerInfo.total_time,
               isRunning: userTimerInfo.is_running,
-              startTime: userTimerInfo.start_time,
             });
           }
           groupSpace.to(groupId).emit('getUsers', nickObjs[groupId]);
-
           // socket.broadcast.to(groupId).emit('user-connected', userId);
         }
         // socket.broadcast.emit('user-connected', userId);
@@ -73,7 +53,6 @@ module.exports = function (io) {
         cb({ isOk: false, error: error.message });
       }
     });
-
     socket.on('zoomJoin', peerId => {
       socket.broadcast.emit('user-connected', peerId);
     });
@@ -85,7 +64,6 @@ module.exports = function (io) {
     // 사용자가 연결을 해제하는 것을 처리합니다.
     socket.on('disconnect', async () => {
       const user = await userController.deleteUser(socket.id);
-
       // 유저가 떠날 때 nickObjs 값에서 삭제
       const arrayToRemoveFrom = nickObjs[groupId];
       if (arrayToRemoveFrom && arrayToRemoveFrom.length > 0) {
@@ -93,7 +71,6 @@ module.exports = function (io) {
         nickObjs[groupId] = updatedArray;
         groupSpace.to(groupId).emit('leaveRoom', nickObjs[groupId]);
       }
-
       console.log('사용자가 소켓 연결을 해제했습니다');
     });
   });
